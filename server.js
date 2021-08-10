149a561ecb32f1c0c0343f44eb2c4b2c997eaac2
fatemeh <fatemehveysi1378@gmail.com>
2021-08-10 22:27:02 +0430

    saving a data in database and using model folder for connection to database and routs folder for seperating different pages

--- server.js
+++ server.js
@@ -2,15 +2,22 @@ const express = require('express')
 const app =express()
 const path = require('path')
 
+const bodyParser = require('body-parser')
+const home = require('./routs/home')
+
+/******************************************MiddleWares********************************************/
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use('/',home)
+
 app.use("/static", express.static('./static/js'))
 app.use("/static", express.static('./static/css'))
 app.use("/static", express.static('./static/fonts'))
 app.use("/static", express.static('./static/scss'))
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/index.html'))
-   
-})
+
+
 
 app.listen(4000,'127.0.0.1',()=>{
     console.log("server is running on port 4000")
